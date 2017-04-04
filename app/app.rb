dc30f0b98189fd48f1284a96a84b0e5fe6d3b2f4
Nick Rupp <nickrupp95@gmail.com>
2017-04-04 13:05:49 +0100

    Add user story 2, add address functionality

--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,22 @@ require './app/models/link'
 
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/links' do
     @links = Links.all
     erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    new_title = params[:title]
+    new_url = params[:url]
+    Links.create(url: new_url, title: new_title)
+    redirect('/links')
+  end
 end
--- spec/features/add_link_spec.rb
+++ spec/features/add_link_spec.rb
@@ -1,10 +1,8 @@
 feature "Add site's address and title to the manager" do
 
   scenario 'Have ability to add new link' do
-    visit('/links/new') do
+      visit('/links/new')
       expect(page).to have_content 'Add Link'
     end
-
-  end
 
 end
