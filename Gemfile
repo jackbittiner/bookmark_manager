d4c1dcc01a97d4f42f32f0f29a614f35e78e17c0
Nick Rupp <nickrupp95@gmail.com>
2017-04-04 11:13:22 +0100

    Update file structure, create first feature test

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'capybara'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'rspec'
+gem 'pry'
---
+++ spec/features/list_link_spec.rb
@@ -0,0 +1,17 @@
+feature 'See a list of links on the homepage' do
+
+  scenario 'Homepage should contain a list of all saved links' do
+
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+    visit('/links')
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+    
+  end
+
+end
