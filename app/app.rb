8aedda0de607300f2c84c2dbdfd071a76baa8e90
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-04 15:44:12 +0100

    change database useage dependant on environment

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require './app/models/link'
-
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
--- spec/features/add_link_spec.rb
+++ spec/features/add_link_spec.rb
@@ -2,7 +2,7 @@ feature "Add site's address and title to the manager" do
 
   scenario 'Have ability to add new link' do
       visit('/links/new')
-      expect(page).to have_content 'Add Link'
+      expect(page).to have_content 'Add link'
   end
 
   scenario 'Add specific info for a link' do
